421402affc97196364b29e17b96700b41a966d74
Georgios Anastasiadis <109350861+anapgeo@users.noreply.github.com>
2024-06-14 00:43:02 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Alegreya",
+        body: "EB Garamond",
         code: "IBM Plex Mono",
       },
       colors: {
