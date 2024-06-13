4f7d9cbcc9db88ba1a60143b8676610187e2c432
Georgios Anastasiadis <109350861+anapgeo@users.noreply.github.com>
2024-06-14 00:57:38 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "LXGW WenKai TC",
         body: "LXGW WenKai TC",
         code: "IBM Plex Mono",
       },
