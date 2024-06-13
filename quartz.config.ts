0c89728415dea15eed87e42d5fd775cd2b95c52f
Georgios Anastasiadis <109350861+anapgeo@users.noreply.github.com>
2024-06-14 00:31:42 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Σημειώσεις Αλγορίθμων",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
