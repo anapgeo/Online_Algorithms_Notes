f8b7f888be16d237a28866a2f40dcd045fba4454
Georgios Anastasiadis <anastasiadis.george1512@gmail.com>
2026-07-06 13:43:44 +0300

    Edit static.yml

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "anapgeo.github.io/Online_Algorithms_Notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
